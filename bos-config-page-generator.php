d495b9f5d8fad7a0cd9217d9e73cc277d911c3f3
Óscar Santiago <bosglunder@gmail.com>
2019-07-06 20:52:27 +0100

    first commit

---
+++ bos-config-page-generator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Bos Config Page Generator
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     bos-config-page-generator
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bos_Config_Page_Generator
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Bos_Config_Page_Generator
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
