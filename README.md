eaf6f45c61a5183fe64529c26df7f0179a3cbb05
Óscar Santiago <bosglunder@gmail.com>
2019-07-07 11:17:41 +0100

    Modify documentation files.
       - Create README.md
    
       - Modify readme.txt
    
       - Modify plugin main file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Config Pages Generator
+
+An easy way to generate a WordPress config page using code.
+
+## Installation
+
+1. Clone it into `/wp-content/plugins/` directory.
+2. Run `npm run install`.
+3. Activate the plugin through the 'Plugins' menu in WordPress. 
--- bos-config-page-generator.php
+++ bos-config-page-generator.php
@@ -2,11 +2,9 @@
 /**
  * Plugin Name:     Bos Config Page Generator
  * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Description:     An easy way to generate a WordPress config page using code.
+ * Author:          Óscar Santiago
  * Text Domain:     bos-config-page-generator
- * Domain Path:     /languages
  * Version:         0.1.0
  *
  * @package         Bos_Config_Page_Generator
